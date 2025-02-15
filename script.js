d6feb0c675f0d0542c0f317a12424cd3118afc03
Eduk <ekakhrisnanda@gmail.com>
2025-02-15 19:41:58 +0800

    feat: Handle the winning condition in checkWinner function
    - Add gameOver variable to check game over condition after game
    - Add conditional check for every cells that has particular pattern and value
    - Add additonal check to print the name from the value of player and make gameOver into true
    - Check the gameOver before move in playRound()
    - Store the variable of the winner, print the name, and stop execution

--- script.js
+++ script.js
@@ -65,8 +65,68 @@ function GameController(
         console.log(`${getActivePlayer().name}'s turn`);
     };
 
+    let gameOver = false;
+
+    const checkWinner = () => {
+        const currentBoard = board.getBoard();
+
+        for(let i = 0; i < currentBoard.length; i++) {
+            // check row
+            if (currentBoard[i][0].getValue() !== '' &&
+                currentBoard[i][0].getValue() === currentBoard[i][1].getValue() &&
+                currentBoard[i][1].getValue() === currentBoard[i][2].getValue()
+            ) {
+                if(currentBoard[i][0].getValue() === getActivePlayer().value) {
+                    gameOver = true;
+                    return getActivePlayer().name;
+                }
+            }
+
+            // check column
+            if (currentBoard[0][i].getValue() !== '' &&
+                currentBoard[0][i].getValue() === currentBoard[1][i].getValue() &&
+                currentBoard[1][i].getValue() === currentBoard[2][i].getValue()
+            ) {
+                if(currentBoard[0][i].getValue() === getActivePlayer().value) {
+                    gameOver = true;
+                    return getActivePlayer().name;
+                }
+            }
+        }
+
+        // check top-left to bottom-right diagonal
+        if (currentBoard[0][0].getValue() !== '' &&
+            currentBoard[0][0].getValue() === currentBoard[1][1].getValue() &&
+            currentBoard[1][1].getValue() === currentBoard[2][2].getValue()
+        ) {
+            if(currentBoard[0][0].getValue() === getActivePlayer().value) {
+                gameOver = true;
+                return getActivePlayer().name;
+            }
+        }
+
+        // check top-right to bottom-left diagonal
+        if (currentBoard[0][2].getValue() !== '' &&
+            currentBoard[0][2].getValue() === currentBoard[1][1].getValue() &&
+            currentBoard[1][1].getValue() === currentBoard[2][0].getValue()
+        ) {
+            if(currentBoard[0][2].getValue() === getActivePlayer().value) {
+                gameOver = true;
+                return getActivePlayer().name;
+            }            
+        }
+    }
+
     const playRound = (row, col) => {
-        if(board.updateCell(row, col, getActivePlayer().value)) {
+        if (gameOver) {
+            return;
+        } else if (board.updateCell(row, col, getActivePlayer().value)){
+            const winner = checkWinner();
+
+            if (winner) {
+                console.log(`${winner} wins!`);
+                return;
+            }
             switchPlayerTurn();
             printNewRound();
         }
